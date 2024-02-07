630b96ddbb315cc10a961e280f7ce3d61ea97f04
sethjr19 <sethyap@gmail.com>
2024-02-07 16:56:23 +0800

    Gojo transition time

--- shingles/src/Components/Gojo.jsx
+++ shingles/src/Components/Gojo.jsx
@@ -12,7 +12,7 @@ const Gojo = () => {
       <span style={{
           transform: isInView ? "none" : "translateX(-200px)",
           opacity: isInView ? 1 : 0,
-          transition: "all 0.3s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+          transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
         }}><img src="./src/assets/gojosatoru.jpg" alt="Gojo" className='gojo-image' />  </span>
     </div>
   )
