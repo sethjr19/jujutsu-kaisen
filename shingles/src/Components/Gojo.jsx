d3c68147114eedd824e38d366be1925f72fb82d6
sethjr19 <sethyap@gmail.com>
2024-02-07 17:55:33 +0800

    Gojo page

--- shingles/src/Components/Gojo.jsx
+++ shingles/src/Components/Gojo.jsx
@@ -9,11 +9,14 @@ const Gojo = () => {
 
   return (
     <div ref={ref} className='gojo-container'>
-      <span style={{
+      <div style={{
           transform: isInView ? "none" : "translateX(-200px)",
           opacity: isInView ? 1 : 0,
           transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
-        }}><img src="./src/assets/gojosatoru.jpg" alt="Gojo" className='gojo-image' />  </span>
+        }}><img src="./src/assets/gojosatoru.jpg" alt="Gojo" className='gojo-image' />  </div>
+        <div className='gojo-text'>
+          <h1>Gojo Satoru</h1>
+        </div>
     </div>
   )
 }
